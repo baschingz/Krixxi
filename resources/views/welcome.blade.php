352e0d385228de616f51e1a2f60022973996cae7
baschingz <36448854+baschingz@users.noreply.github.com>
2018-07-02 21:40:47 +0700

    bas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.2/angular.min.js"></script>
+		<!-- Latest compiled and minified CSS -->			
         <title>Krixxi</title>
 
         <!-- Fonts -->
@@ -16,13 +17,29 @@
 		
         <div id="div1" class="font1" hidden>Krixxi </div>
 		<div id="div2" class="font2" hidden>keep memory</div>
+		<div>
+		<button id="bnt1" class="btn1" >test</button>
+		</div>
         
+		<div id="login" hidden>
+		<label>username :</label>
+		<input type="text"><br>
+		<label>password :</label>
+		<input type="text"><br>
+		<input type="button">
+		</div>
     </body>
-        
 		<script>
 			$(function(){			
 			$('#div1').slideDown("slow");
 			$('#div2').slideDown("slow");
+			
+			$('#bnt1').click(function(){
+				$('#div1').hide( "slow" );
+				$('#div2').hide( "slow" );
+				$('#bnt1').hide( "slow" );
+				$('#login').slideDown("slow");
+			});
 			});		
 		</script>
 </html>
