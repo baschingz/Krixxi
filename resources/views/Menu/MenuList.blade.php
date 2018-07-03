9a867a8d65b13a412a827cb94dae9608dc856c21
jiidapa <jidapa.p@kkumail.com>
2018-07-03 09:47:06 +0700

    icon title

--- resources/views/Menu/MenuList.blade.php
+++ resources/views/Menu/MenuList.blade.php
@@ -1,16 +1,8 @@
 @extends('layout.main')
 @section('body')
-<h1>test</h1>
 <div class="container-fluid">
 
-    <div class="row">
-        <div class="col-xs-6" stlye="background-color:red;">test</div>
-        <div class="col-xs-6" stlye="background-color:red;">test</div>
-    </div>
-    <div class="row">
-        <div class="col-xs-6" stlye="background-color:red;">test</div>
-        <div class="col-xs-6" stlye="background-color:red;">test</div>
-    </div>
+    
 
 </div>
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,30 +5,35 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Krixxi</title>
+
+        <link rel="icon" href="{{asset('image/icon/Icon-20.png')}}" type="image/x-icon" />
         <!-- Fonts -->        
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="{{asset('css/background.css')}}"/> 
-        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}"/> 
-       
+        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}"/>
+        
+        <link href="https://fonts.googleapis.com/css?family=Kanit:200" rel="stylesheet">
         <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"> -->
         <!-- Styles -->
         
     </head>
     <body >      
     <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-header">
-        <a class="navbar-brand" href="menu">Krixxi</a>
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <a class="navbar-brand" href="menu">Krixxi</a>
+            </div>
+            <ul class="nav navbar-nav">
+            <li><a  class="glyphicon glyphicon-th-large" href="menu"></a></li>
+            <li><a class="glyphicon glyphicon-user" href="#"></a></li>
+            <li><a class="glyphicon glyphicon-comment" href="#"></a></li>
+            <li><a class="glyphicon glyphicon-search" href="#"></a></li>
+            </ul>
         </div>
-        <ul class="nav navbar-nav">
-        <li><a  class="glyphicon glyphicon-th-large" href="menu"></a></li>
-        <li><a class="glyphicon glyphicon-user" href="#"></a></li>
-        <li><a class="glyphicon glyphicon-comment" href="#"></a></li>
-        <li><a class="glyphicon glyphicon-search" href="#"></a></li>
-        </ul>
-    </div>
     </nav>
     <br>
+
+
         @yield('body')
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.2/angular.min.js"></script>
