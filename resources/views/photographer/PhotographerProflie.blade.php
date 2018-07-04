1df95deed537ead4d31df26348a1617cae6e6f47
thanyakan <Oopoip1394@gmail.com>
2018-07-04 23:56:28 +0700

    commit

--- resources/views/photographer/PhotographerProflie.blade.php
+++ resources/views/photographer/PhotographerProflie.blade.php
@@ -1,6 +1,21 @@
 @extends('layout.main')
 @section('body')
-
+    <div style="margin-top:-5%;">
+    <img style="width:100%; height:50%;" src="{{asset('image/Profile_ex/header_ex.jpg')}}">
+        <div>
+        <div class="container-fluid" style="background-color:white; padding-left:0; padding-right:0;">
+        <div class="col-md-3">
+        <div style="margin-left:30%; margin-right:20%; margin-top:15;">
+        <img src="{{asset('image/Profile_ex/avatar_ex.png')}}" class="img-circle">
+        </div>
+        <div class="col-md-4">
+         detail
+        </div>
+        <div class="col-md-4">
+        rate
+        </div>
+        </div>
+    </div>
 @endsection
 <div ng-controller="PhotographerController" ng-init="init({{$id}})">
 
