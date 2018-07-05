6fb57aff21b0b3fa13ac7f55f90af99fb5a62d2f
baschingz <36448854+baschingz@users.noreply.github.com>
2018-07-05 13:22:02 +0700

    commit

--- public/js/Search/SearchJS.js
+++ public/js/Search/SearchJS.js
@@ -8,16 +8,20 @@ app.controller('SearchController', function ($scope, $http) {
     $scope.photographyList = typeof searchValue !== "undefined" ? searchValue ? searchValue : null : null;
     $scope.searchValue = {
         location: '',
-        rate: '',
+        cost: null,
         service: '',
         date: ''
     };
+    $scope.location = typeof location != "undefined" ? location ? location : null : null;
+    $scope.cost = typeof cost != "undefined" ? cost ? cost : null : null;
+    $scope.service = typeof service != "undefined" ? service ? service : null : null;
+
     $scope.searchPG = function (e) {
         debugger;
         var path = base_path + '/photolist/search';
-        var req ={
-            method:'POST',            
-            url:path,
+        var req = {
+            method: 'POST',
+            url: path,
             data: e
         }
         $http(req).then(function (response) {
@@ -25,11 +29,26 @@ app.controller('SearchController', function ($scope, $http) {
                 alert(response.data);
             },
             function (response) {
-                debugger;
+                // debugger;
                 alert('fail');
             }
         );
     };
+
+    $scope.init = function (page) {
+        page = page || null;
+        path = base_path + '/getselect';
+
+        $http.get(path).then(function (response) {
+            
+            $scope.location = response.data[0];
+            $scope.cost = response.data[1];
+            $scope.service = response.data[2];
+           
+        }, function (response) {
+            alert('fail');
+        });
+    }
 
 });
 
