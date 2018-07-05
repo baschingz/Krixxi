37f0886ec1bb87f02254dd165fc9b69685a2839d
jiidapa <jidapa.p@kkumail.com>
2018-07-05 16:01:20 +0700

    welcome edit

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -14,12 +14,10 @@
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}"/>
     <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}"/>
     <link href="https://fonts.googleapis.com/css?family=Kanit:100,200,300" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Yantramanav:100" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Kanit:400,500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Yantramanav:100,200" rel="stylesheet">
 
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"> -->
     <!-- Styles -->
-
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,regular,500,600,700&subset=latin-ext,vietnamese,latin,cyrillic"
           rel="stylesheet" type="text/css"/>
     @yield('import')
@@ -53,28 +51,28 @@
     @yield('body')
 </nav>
 <section style="margin-top:39%;">
-<footer class="footer navbar-bottom">
-    <div class="container">
-        <div class="col-sm-12 col-sm-offset-1">
-            <div class="col-sm-4 logo-size">
-                Krixxi
-            </div>
-            <div class="col-sm-4">
-                <div class="footer_contact">
-                    <ul class="nav">
-                        <li>Contact us</li>
-                        <li>E-mail : Krixxi@gmail.com</li>
-                        <li>Tel : 098-107-5755</li>
-                        <li>Facebook : Krixxi แหล่งรวมช่างภาพ</li>
-                    </ul>
+    <footer class="footer navbar-bottom">
+        <div class="container">
+            <div class="col-sm-12 col-sm-offset-1">
+                <div class="col-sm-4 logo-size">
+                    Krixxi
+                </div>
+                <div class="col-sm-4">
+                    <div class="footer_contact">
+                        <ul class="nav">
+                            <li>Contact us</li>
+                            <li>E-mail : Krixxi@gmail.com</li>
+                            <li>Tel : 098-107-5755</li>
+                            <li>Facebook : Krixxi แหล่งรวมช่างภาพ</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-4 footer_copyright">
+                    &copy; copyright Krixxi 2018
                 </div>
             </div>
-            <div class="col-sm-4 footer_copyright">
-                &copy; copyright Krixxi 2018
-            </div>
         </div>
-    </div>
-</footer>
+    </footer>
 </section>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.2/angular.min.js"></script>
