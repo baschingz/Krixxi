356511c80717a3beafe86145387a4ee03724cfb9
jiidapa <jidapa.p@kkumail.com>
2018-07-06 14:39:18 +0700

    search

--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -1,13 +1,16 @@
 @extends('layout.main')
 @section('import')
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="{{asset('css/search.css')}}">
 
 @endsection
 @section('body')
 
+<section class="bg-search">
 <div ng-controller="SearchController">
     <div ng-form ng-init="init()">
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-sm-10 col-sm-offset-1">
             <div class="containerSearch col-sm-10 col-sm-offset-1" hidden id="search" >
                 <div class="panelSearch" >
                     <div class="panel-headingSearch form-center font-topic">ค้นหา</div>
@@ -64,19 +67,32 @@
             </div>
         </div>
             
-        
         <!-- <div ng-repeat="obj in photographyList"></div> -->
-        <div class="container">
-        <div ng-if="check">
-            <div class="container-fluid">
-                <div class="container">
-                   
-                </div>
-            </div>
-        </div>
-        </div>
+        
     </div>
 </div>
+</section>
+<setion>
+    <div class="container">
+        <div class="card hovercard">
+            <img src="http://placehold.it/300x200/000000/&text=Header" alt=""/>
+            <div class="avatar">
+                <img src="http://placehold.it/80X80/333333/&text=Head" alt="" />
+            </div>
+            <div class="info">
+                <div class="title">
+                    The Title
+                </div>
+                <div class="desc">Lorem ipsum</div>
+                <div class="desc">Lorem ipsum</div>
+                <div class="desc">Lorem ipsum</div>
+            </div>
+            <div class="bottom">
+                <button class="btn btn-default">Button</button>
+            </div>
+        </div>
+    </div>
+</setion>
 @endsection
 
 @section('script')
