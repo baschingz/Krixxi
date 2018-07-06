50903f0c9975d4ea26936ec4605ffd29e332c14b
thanyakan <Oopoip1394@gmail.com>
2018-07-06 09:05:32 +0700

    commit

--- resources/views/photographer/PhotographerProflie.blade.php
+++ resources/views/photographer/PhotographerProflie.blade.php
@@ -1,21 +1,38 @@
 @extends('layout.main')
 @section('body')
-    <div style="margin-top:-5%;">
-    <img style="width:100%; height:50%;" src="{{asset('image/Profile_ex/header_ex.jpg')}}">
-        <div>
-        <div class="container-fluid" style="background-color:white; padding-left:0; padding-right:0;">
-        <div class="col-md-3">
-        <div style="margin-left:30%; margin-right:20%; margin-top:15;">
-        <img src="{{asset('image/Profile_ex/avatar_ex.png')}}" class="img-circle">
+    <div class="card" style="margin-top:-5%;">
+  <img class="card-img" src="{{asset('image/Profile_ex/header_ex.jpg')}}" style="height:60%;" >
+    </div>
+    <div class="container-fluid" style="padding-top:10; padding-bottom:20;">
+        <div class="col-md-3 col-md-offset-1">
+            <img src="{{asset('image/Profile_ex/avatar_ex.png')}}" style="height:256; width:256;">
         </div>
-        <div class="col-md-4">
-         detail
+        <div class="col-md-4" style="padding-top:3%;">
+            <div class="h1">TOMMY</div>
+            <div style="font-size:22;">Detail</div>
+            <ul class="list-unstyled" style="font-size:18;">
+            <li>Name    :   Thanita</li>
+            <li>TEL     :   08x-xxx-xxxx</li>
+         </div>
+         <div class="col-md-3" style="padding-top:8%;">
+            <div style="font-size:22;">Rate</div>
+            <ul class="list-unstyled" style="font-size:18;">
+            <li>Half    :  2500</li>
+            <li>Full    :  3000</li>
+         </div>
+    </div>
+    <div class="card-deck">
+        <div class="card">
+            <img class="card-img"  src="{{asset('image/Profile_ex/albam1.jpg')}}" style="height:256;">
         </div>
-        <div class="col-md-4">
-        rate
+        <div class="card">
+            <img class="card-img" src="{{asset('image/Profile_ex/albam2.jpg')}}" style="height:256;">
         </div>
+        <div class="card">
+            <img class="card-img" src="{{asset('image/Profile_ex/albam3.jpg')}}" style="height:256;">
         </div>
     </div>
+
 @endsection
 <div ng-controller="PhotographerController" ng-init="init({{$id}})">
 
