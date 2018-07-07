73cfa81804a7cd70663535e600b2621b3771eb40
thanyakan <Oopoip1394@gmail.com>
2018-07-07 12:47:07 +0700

    commit

--- resources/views/photographer/PhotographerProflie.blade.php
+++ resources/views/photographer/PhotographerProflie.blade.php
@@ -5,7 +5,7 @@
     </div>
     <div class="container-fluid" style="padding-top:10; padding-bottom:20;">
         <div class="col-md-3 col-md-offset-1">
-            <img src="{{asset('image/Profile_ex/avatar_ex.png')}}" style="height:256; width:256;">
+            <img src="{{asset('image/Profile_ex/avatar_ex.png')}}" class="img-circle" style="height:256; width:256;">
         </div>
         <div class="col-md-4" style="padding-top:3%;">
             <div class="h1">TOMMY</div>
@@ -33,11 +33,54 @@
         </div>
     </div>
 
+    <div class="container-fiuid" style="margin-top:30;">
+        <div class="col-md-6 col-md-offset-3">
+            <label class="comment-label">Comments</label>
+            <div class="comments-list">
+            <div class="media">
+            <div class="media-left media-top">
+                <img class="img-circle" src="{{asset('image/Profile_ex/avatar_ex.png')}}" style="width:64;">
+            </div>
+                <div class="media-body">
+                <h4 class="media-heading">Username</h4>
+                   hello
+                </div>
+                <div class="comment-time">07/07/2018 12:28 PM</div>
+                </div>
+                <div class="media">
+            <div class="media-left media-top">
+                <img class="img-circle" src="{{asset('image/Profile_ex/avatar_ex.png')}}" style="width:64;">
+            </div>
+                <div class="media-body">
+                <h4 class="media-heading">Username</h4>
+                   Beautiful
+                </div>
+                <div class="comment-time">07/07/2018 12:28 PM</div>
+                </div>
+            </div>
+            <div class="media">
+            <div class="media-left media-top">
+                <img class="img-circle" src="{{asset('image/Profile_ex/avatar_ex.png')}}" style="width:64;">
+            </div>
+                <div class="media-body">
+                <h4 class="media-heading">Username</h4>
+                <textarea class="form-control" rows="3" placeholder="Comment"></textarea>
+                <div style="margin-top:5px;">
+                <button type="button" class="btn-comment">Send</button>
+                </div>
+            </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
 @endsection
 <div ng-controller="PhotographerController" ng-init="init({{$id}})">
 
 </div>
 @section('script')
-
 <script src="{{asset('js/Photographer/PhotographerJS.js')}}"></script>
 @endsection
