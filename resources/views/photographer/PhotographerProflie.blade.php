f624207c3da9d1d139ba7c306a5bc88c8858dde5
baschingz <36448854+baschingz@users.noreply.github.com>
2018-07-07 16:23:24 +0700

    commit

--- resources/views/photographer/PhotographerProflie.blade.php
+++ resources/views/photographer/PhotographerProflie.blade.php
@@ -1,24 +1,29 @@
 @extends('layout.main')
 @section('body')
-    <div class="card" style="margin-top:-5%;">
-  <img class="card-img" src="{{asset('image/Profile_ex/header_ex.jpg')}}" style="height:60%;" >
+
+    <!-- ng-repeat="obj in photographer " -->
+
+@endsection
+<div ng-controller="PhotographerController" ng-init="init({{$id}})" >
+<div class="card" style="margin-top:-5%;" >
+  <img class="card-img" src="<% photographer[0].cover_img %>" style="height:60%;" >
     </div>
     <div class="container-fluid" style="padding-top:10; padding-bottom:20;">
         <div class="col-md-3 col-md-offset-1">
-            <img src="{{asset('image/Profile_ex/avatar_ex.png')}}" style="height:256; width:256;">
+            <img src="<% photographer[0].profile_img %>" style="height:256; width:256;">
         </div>
         <div class="col-md-4" style="padding-top:3%;">
-            <div class="h1">TOMMY</div>
-            <div style="font-size:22;">Detail</div>
+            <div class="h1"><% photographer[0].penname %></div>
+            <div style="font-size:22;">Detail : <% photographer[0].description %></div>
             <ul class="list-unstyled" style="font-size:18;">
-            <li>Name    :   Thanita</li>
-            <li>TEL     :   08x-xxx-xxxx</li>
+            <li>Name    :  <% photographer[0].fullname %></li>
+            <li>ภูมิภาค     :   <% photographer[0].name %></li>
          </div>
          <div class="col-md-3" style="padding-top:8%;">
             <div style="font-size:22;">Rate</div>
             <ul class="list-unstyled" style="font-size:18;">
-            <li>Half    :  2500</li>
-            <li>Full    :  3000</li>
+            <li>Half    :  <% photographer[0].parttime %></li>
+            <li>Full    :  <% photographer[0].fulltime %></li>
          </div>
     </div>
     <div class="card-deck">
@@ -32,10 +37,6 @@
             <img class="card-img" src="{{asset('image/Profile_ex/albam3.jpg')}}" style="height:256;">
         </div>
     </div>
-
-@endsection
-<div ng-controller="PhotographerController" ng-init="init({{$id}})">
-
 </div>
 @section('script')
 
